4ec3fc516c67ed194420e16f3d2825ce045264e2
giyos <rashidovgiyos409@gmail.com>
2024-01-18 14:44:55 +0500

    a

--- src/Components/Superiority.tsx
+++ src/Components/Superiority.tsx
@@ -90,9 +90,9 @@ const Superiority = () => {
 
                 </div>
 
-               {!isButtonVisible && <div className='block fixed bottom-5 z-[60] left-1/2 -translate-x-[50%]'>
+               {!isButtonVisible && <a href='tel:+998 95-157-50-50' className='block fixed bottom-5 z-[60] left-1/2 -translate-x-[50%]'>
                     <button className='bg-mainColor w-full py-4 px-6 rounded-lg text-white flex items-center gap-2 mx-auto'><FaPhoneAlt /> <span className='font-medium text-xs sm:text-base'>Вызвать мастера</span></button>
-                </div>}
+                </a>}
             </div>
         </section>
     )
