9f7fba1f67ddb7356ac8d56a5c7dde53e262e4e3
giyos <rashidovgiyos409@gmail.com>
2024-01-18 14:42:26 +0500

    meta tags

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,16 +1,30 @@
 import React from 'react'
 import { CiLocationOn } from "react-icons/ci";
 import { FaPhoneAlt, FaTelegram, FaInstagram } from "react-icons/fa";
+
+import {useForm } from 'react-hook-form';
+
+type Inputs = {
+    phoneNumber: string | number
+}
+
 const Contact = () => {
+  const {register, handleSubmit, formState:{errors}}  = useForm<Inputs>()
+
+  const submitHandler = (e: Inputs) => {
+      console.log(e);
+      
+  }
+
   return (
     <section id='contact'>
       <div className="container">
         <div className="bg-[#F9FAFB] rounded-2xl py-10 px-6 md:py-16 text-center">
           <p className='text-[28px] text-5xl text-darkColor font-russo font-normal leading-[44px] -tracking-[0.96px]'>Оставьте заявку</p>
           <p className='text-lg md:text-xl text-lightColor font-jura font-normal leading-[28px] md:leading-[30px] mt-5 max-w-[768px] mx-auto'>На вызов мастера по ремонту стоимость вызова котлов и услуги при заказе с сайта - дополнительная скидка 10%. Мастер перезвонит Вам в течение 10 минут! Вызов мастера стоить 90.000 UZS</p>
-          <form action="">
+          <form onSubmit={handleSubmit(submitHandler)}>
             <div className='flex flex-col md:flex-row items-center justify-center gap-4 mt-8'>
-              <input type='text' placeholder='Номер телефона' />
+              <input type='text' placeholder='Номер телефона' {...register('phoneNumber', {required: true})}/>
               <button type='button' className='bg-mainColor w-full md:max-w-[173px] font-jura text-white text-base font-medium py-3 px-5 rounded-lg'>Оставить заявку</button>
             </div>
           </form>
--- src/Components/Showcase.tsx
+++ src/Components/Showcase.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { showcase_img } from '../Assets/Images';
-import { useForm , SubmitHandler} from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import { instance } from '../instance/instance';
 
 type Inputs = {
-    name: string
+    username: string
     exampleRequired: string
     phoneNumber: string
+    field: number | string
 }
 
 const Showcase = () => {
+
     const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
 
-    const onSubmitHandler:SubmitHandler<Inputs> = (e) => {
-        console.log(e);
+    const onSubmitHandler = (e: Inputs) => {
+        
     }
+    useEffect(() => {
+        // instance.get('/todos').then(res => console.log(res.data)
+        // );
+    }, []);
+
 
     return (
         <section id='showcase' className='pt-16 md:pt-24'>
@@ -22,9 +30,9 @@ const Showcase = () => {
                 <p className='font-jura text-lightColor text-center text-xs md:text-xl font-normal leading-7 md:leading-[36px] max-w-[768px] mx-auto mt-4 md:mt-6 mb-8 md:mb-12'>Наша компания специализируется на обслуживании и ремонте газовых котлов различных марок и моделей. Мы имеем в наличии все необходимые запчасти и комплектующие для быстрого и эффективного ремонта.</p>
                 <form onSubmit={handleSubmit(onSubmitHandler)}>
                     <div className='flex flex-col md:flex-row items-center justify-center gap-4'>
-                        <input type='text' placeholder='Имя' className='font-jura' {...register('name', {validate: {}})} />
-                        <input type='text' placeholder='Номер телефона' className='font-jura' />
-                        <button type='button' className='bg-mainColor w-full md:max-w-[173px] font-jura text-white text-base font-medium py-3 px-5 rounded-lg'>Оставить заявку</button>
+                        <input type='text' placeholder='Имя' className='font-jura' {...register('username', { required: { value: true, message: 'Iltimos ismingizni kiriting!' } })} />
+                        <input type='number' placeholder='Номер телефона' className='font-jura' {...register('phoneNumber', { required: { value: true, message: 'Telefon raqamingizni kiriting!' } })} />
+                        <button type='submit' className='bg-mainColor w-full md:max-w-[173px] font-jura text-white text-base font-medium py-3 px-5 rounded-lg'>Оставить заявку</button>
                     </div>
                 </form>
                 <div className='mt-16'>
